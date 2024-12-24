cbe0f6d7b5427001e48bffdfa51ce8be1417247a
SashaMedved <laskov98rus@list.ru>
2024-12-25 01:56:12 +0500

    Created ASP

---
+++ Asp.Net/UrbanOut/UrbanOut/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UrbanOut.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
