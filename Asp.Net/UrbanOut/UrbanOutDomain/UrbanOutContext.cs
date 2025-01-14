75214c39bbb97487eafefab78d81e362b8699b69
Александр Чернов <laskov98rus@list.ru>
2025-01-14 10:15:07 +0500

    bag fix x2

---
+++ Asp.Net/UrbanOut/UrbanOutDomain/UrbanOutContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrbanOutDomain.Entities;
+
+namespace UrbanOutDomain
+{
+    public class UrbanOutContext :DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UrbanOutContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+    }
+}
